2f466cbab6de8f5a62b5a0c1d6e97416fc052475
minju412 <ln8847@ajou.ac.kr>
2024-01-19 00:36:47 +0900

    [refactor] continue로 if문 depth 줄이기

--- ChoiMinJu/programmers/doll_game.java
+++ ChoiMinJu/programmers/doll_game.java
@@ -8,20 +8,17 @@ class Solution {
         Stack<Integer> stk = new Stack<>();
         for (int e : moves) {
             for (int i=0; i<N; i++) {
-                if (board[i][e-1] != 0) {
-                    stk.push(board[i][e-1]);
-                    if (stk.size() >= 2) {
-                        int topElement = stk.pop();
-                        if (topElement == stk.peek()) {
-                            stk.pop();
-                            cnt += 2;
-                        } else {
-                            stk.push(topElement); // 뽑은거 다시 넣기
-                        }
-                    }
-                    board[i][e-1] = 0;
-                    break;
+                if (board[i][e-1] == 0) continue;
+                stk.push(board[i][e-1]);
+                if (stk.size() < 2) continue;
+                if (topElement == stk.peek()) {
+                    stk.pop();
+                    cnt += 2;
+                } else {
+                    stk.push(topElement); // 뽑은거 다시 넣기
                 }
+                board[i][e-1] = 0;
+                break;
             }
         }      
         return cnt;
