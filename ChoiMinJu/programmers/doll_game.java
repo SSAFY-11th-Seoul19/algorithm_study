b4026c6a1619143cf640fa1469115831146db0ec
minju412 <59405576+minju412@users.noreply.github.com>
2024-01-19 00:37:32 +0900

    최민주 / 1월 3주차 / 목요일  (#12)
    * Create .gitkeep
    
    * Create doll_game.java
    
    [solved] doll_game
    
    * [refactor] continue로 if문 depth 줄이기

---
+++ ChoiMinJu/programmers/doll_game.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+
+    public int solution(int[][] board, int[] moves) {
+        int N = board.length;
+        int cnt = 0;
+        Stack<Integer> stk = new Stack<>();
+        for (int e : moves) {
+            for (int i=0; i<N; i++) {
+                if (board[i][e-1] == 0) continue;
+                stk.push(board[i][e-1]);
+                if (stk.size() < 2) continue;
+                if (topElement == stk.peek()) {
+                    stk.pop();
+                    cnt += 2;
+                } else {
+                    stk.push(topElement); // 뽑은거 다시 넣기
+                }
+                board[i][e-1] = 0;
+                break;
+            }
+        }      
+        return cnt;
+    }
+}
