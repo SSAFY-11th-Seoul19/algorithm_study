99093844b7aa700134adbddce213af476cdebda8
Jay <81621620+HooKim@users.noreply.github.com>
2024-01-23 08:47:04 +0900

    [solved] 6137

--- KimJaeHoo/BOJ/6137.java
+++ KimJaeHoo/BOJ/6137.java
@@ -1,58 +1,56 @@
 import java.util.*;
+
 public class Main {
+    public static void main(String args[]) {
+        int N, beg, end, nextBeg, nextEnd;
+        StringBuilder str = new StringBuilder("");
+        StringBuilder buffer = new StringBuilder("");
 
-	public static void main(String[] args) {
-		int N, beg = 0, end;
-		String S = "", T ="";
-		Scanner in = new Scanner(System.in);
-		
-		N = in.nextInt();
-		for(int i = 0; i < N; i++) {
-			S += in.next().charAt(0);
-		}
-				
-		end = N-1; 
-		
-		while(beg <= end) {
-			char begVal = S.charAt(beg);
-			char endVal = S.charAt(end);
-			
-			if(begVal > endVal) {
-				T += endVal;
-				end--;
-			}
-			else if (begVal < endVal) {
-				T += begVal;
-				beg++;
-			}
-			else if(beg + 1 <= end -1){
-				char begNextVal = S.charAt(beg+1);
-				char endNextVal = S.charAt(end-1);
-				if(begNextVal < endNextVal) {
-					T += begVal;
-					beg++;
-				}
-				else if(begNextVal > endNextVal){
-					T += endVal;
-					end--;
-				}
-				else {
-					T += begVal;
-					beg++;
-				}
-			}
-			else {
-				T += begVal;
-				beg++;
-			}
-			if(T.length() == 80) {
-				System.out.println(T);
-				T = "";
-			}
-		}
-		
-		System.out.println(T);
-		
-	}
+        Scanner in = new Scanner(System.in);
 
+        N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+           str.append(in.next().charAt(0));
+        }
+
+        beg = 0;
+        end = N-1;
+
+        boolean isLeft =true;
+        while(beg <= end){
+            if(str.charAt(beg) < str.charAt(end))
+                buffer.append(str.charAt(beg++));
+            else if(str.charAt(beg) > str.charAt(end))
+                buffer.append(str.charAt(end--));
+            else{
+                int l = beg+1 , r=end-1;
+
+                while(l <= r){
+                    if(str.charAt(l) < str.charAt(r)) {
+                        isLeft = true;
+                        break;
+                    }
+                    else if(str.charAt(l) > str.charAt(r)){
+                        isLeft = false;
+                        break;
+                    }
+                    else{
+                        l++;
+                        r--;
+                    }
+                }
+
+                if(isLeft)
+                    buffer.append(str.charAt(beg++));
+                else
+                    buffer.append(str.charAt(end--));
+
+            }
+            if(buffer.length() == 80){
+                System.out.println(buffer);
+                buffer.setLength(0);
+            }
+        }
+        System.out.println(buffer);
+    }
 }
