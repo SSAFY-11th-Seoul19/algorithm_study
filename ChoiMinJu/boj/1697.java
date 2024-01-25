8b64f40afb1e4517b45ef6abae9066adc06d9ac3
minju412 <59405576+minju412@users.noreply.github.com>
2024-01-25 19:37:22 +0900

    [solved] 1697

---
+++ ChoiMinJu/boj/1697.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int K;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(stk.nextToken());
+        K = Integer.parseInt(stk.nextToken());
+
+        DFS(N, 0);
+        System.out.println(min);
+    }
+
+    public static void DFS(int n, int cnt) {
+        if (cnt > min) return;
+        if (n == K && min > cnt) {
+            min = cnt;
+        }
+        DFS(n+1, cnt+1);
+        DFS(n-1, cnt+1);
+        DFS(n*2, cnt+1);
+    }
+}
