0b04fa3af127f5dcd741d4dc4f8874c08b3f6262
Hyeokjin Kim <80228712+gurwls0122@users.noreply.github.com>
2024-02-29 09:23:20 +0900

    [solved] artistry

--- KimHyeokjin/boj/artistry.py
+++ KimHyeokjin/boj/artistry.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+from collections import deque, defaultdict
 import copy
 n = int(sys.stdin.readline())
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
@@ -10,7 +10,6 @@ def getScore():
     visited = [[0] * n for _ in range(n)]
     group = []
     groupCnt = 0
-    groupReach = [[0] * (n**2) for _ in range(n**2)]
 
     #group으로 나누기(bfs)
     for i in range(n):
@@ -30,18 +29,6 @@ def getScore():
                         if newY<0 or newY>=n or newX<0 or newX>=n:
                             continue
                         #만약, 자신의 양옆에 있는애중에 값이 다르고 방문처리가 되었다면.. 이미저장된 값일테니 찾아서 확인
-                        if board[newY][newX]!=val and visited[newY][newX]:
-                            flag = False
-                            for l in range(groupCnt):
-                                if flag:
-                                    break
-
-                                for node in group[l]:
-                                    if node[0] == newY and node[1] == newX:
-                                        groupReach[l][groupCnt]+=1
-                                        flag = True
-                                        break
-                            continue
 
                         if visited[newY][newX] or board[newY][newX]!=val:
                             continue
@@ -49,16 +36,18 @@ def getScore():
                         visited[newY][newX] = True
                         group[groupCnt].append((newY, newX))
                         queue.append((newY, newX))
-                # print(groupReach)
                 groupCnt+=1
-
+    contactMap = defaultdict(int)
+    for i in range(len(group)-1):
+        for j in range(i+1, len(group)):
+            for k in range(len(group[i])):
+                for l in range(4):
+                    if (group[i][k][0] + dy[l], group[i][k][1] + dx[l]) in group[j]:
+                        contactMap[(i, j)] +=1
     ans = 0
     #점수 구하기
-    for i in range(n):
-        for j in range(n):
-            if groupReach[i][j]==0:
-                continue
-            ans += (len(group[i]) + len(group[j])) * board[group[i][0][0]][group[i][0][1]] * board[group[j][0][0]][group[j][0][1]] * groupReach[i][j]
+    for key, value in contactMap.items():
+        ans += (len(group[key[0]]) + len(group[key[1]])) * board[group[key[0]][0][0]][group[key[0]][0][1]] * board[group[key[1]][0][0]][group[key[1]][0][1]] * value
     return ans
 
 def mix():
@@ -92,15 +81,14 @@ def mix():
             newBoard[j][i] = board[i][j]
 
     board = copy.deepcopy(newBoard)
-    for i in range(n):
-        print(*board[i])
+    # for i in range(n):
+    #     print(*board[i])
 
     # print("=============")
 if __name__=="__main__":
     ans = 0
     ans += getScore()
     for i in range(3):
-        print(ans)
         mix()
         ans+=getScore()
     print(ans)
